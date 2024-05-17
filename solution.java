1848a8984456a2234124e558a0f8ecb5365f5649
Kooreis <kooreis.official@gmail.com>
2024-05-17 15:52:08 -0500

    In this update, the isMatch method is created. It initializes four variables: i, j, starIndex, and iIndex.

--- solution.java
+++ solution.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the string:");
-        String str = scanner.nextLine();
-        System.out.println("Enter the pattern:");
-        String pattern = scanner.nextLine();
+        // ... existing code ...
+    }
+
+    public static boolean isMatch(String s, String p) {
+        int i = 0;
+        int j = 0;
+        int starIndex = -1;
+        int iIndex = -1;
     }
 }
